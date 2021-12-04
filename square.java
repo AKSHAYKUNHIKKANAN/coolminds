4732274d0ac9555a9031e3403e136aef87507faf
AKSHAYKUNHIKKANNAN <akshaykvkk@gmail.com>
2021-12-04 09:03:08 +0530

    the first program has been committeed

---
+++ square.java
@@ -0,0 +1,17 @@
+import java.util.Scanner; //import the scanner class
+class square{
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in); //Creating a object of Scanner class
+        System.out.print("Enter The Number : ");
+        int value = read.nextInt(); //Read the value from keybord using Scanner object
+        if(value>=1 && value<=5) //Checking the lower and upper limit
+        {
+            int square = value * value ; 
+            System.out.println("The square of the given Number is :"+square);
+        }
+        else{
+            System.out.println("The Input is out of the range");
+        }
+
+    }
+}
